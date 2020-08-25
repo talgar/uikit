313ac689049087d6ca21ef0cf4db35402086d329
Talgar <talgarrmelisbekov@gmail.com>
2020-08-25 19:48:52 +0600

    added commit

--- GIT, GitHub, SourceTree, Alerts, UIAlertController/GIT, GitHub, SourceTree, Alerts, UIAlertController/ViewController.swift
+++ GIT, GitHub, SourceTree, Alerts, UIAlertController/GIT, GitHub, SourceTree, Alerts, UIAlertController/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func showAlert() {
+        
+        let alert = UIAlertController(title: "Photo access", message: "Do you allow to use your photos", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+       
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func showActionShip() {
+    }
+    
+    
 }
 
